1b3f1c18f27cf95ebaef150bc119bdac8048eddb
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-24 16:31:55 +0100

    iniziato MAIN

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,15 +1,57 @@
 ﻿// definire qualche funzione di utilità che poi potete usare per poter fare operazioni complesse nei vostri programmi principali.
+// Una volta completate queste funzioni di utilità di base, e dato il seguente array di numeri [2, 6, 7, 5, 3, 9] già dichiarato nel vostro codice
+// si vogliono utilizzare le funzioni per:
+// - Stampare l’array di numeri fornito a video (FATTO)
+// - Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato
+//   (Verificare che l’array originale non sia stato modificato quindi ristampare nuovamente l’array originale e verificare che sia rimasto [2, 6, 7, 5, 3, 9])
+// - Stampare la somma di tutti i numeri (FATTO)
+// - Stampare la somma di tutti i numeri elevati al quadrati
+
+//---------------------------- MAIN ------------------------------------------------------
+
+int[] arrayNumeri = { 2, 6, 7, 5, 3, 9 };
+
+StampaArrayInteri(arrayNumeri);
+
+Quadrato(3);
+
+SommaElementiArray(arrayNumeri);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------DICHIARAZIONE FUNZIONI----------------------------------
 
 // FUNZIONE 1
 
-void StampaArrayInteri(int[] arrayInteri) {
+void StampaArrayInteri(int[] array) {
     Console.Write("[");
 
-    for (int i = 0; i < arrayInteri.Length; i++) {
-        if (i == arrayInteri.Length - 1) {
-            Console.Write(arrayInteri[i]);
+    for (int i = 0; i < array.Length; i++) {
+        if (i == array.Length - 1) {
+            Console.Write(array[i]);
         } else {
-            Console.Write(arrayInteri[i] + ", ");
+            Console.Write(array[i] + ", ");
         }
     }
     Console.WriteLine("]");
@@ -19,11 +61,12 @@ void StampaArrayInteri(int[] arrayInteri) {
 
 int Quadrato(int numero) {
     int numeroQuadrato = numero * numero;
+    Console.WriteLine(numeroQuadrato);
     return numeroQuadrato;
 }
 
 // FUNZIONE 3
-
+/*
 int ElevaArrayAlQuadrato(int[] array) {
 
     int[] arrayQuadratoCopia = (int[])array.Clone();
@@ -34,12 +77,13 @@ int ElevaArrayAlQuadrato(int[] array) {
 
     }
 
+    Console.WriteLine(SommaElementiArray(arrayQuadratoCopia);
     return arrayQuadratoCopia;
 }
-
+*/
 // FUNZIONE 4
 
-int sommaElementiArray(int[] array) {
+int SommaElementiArray(int[] array) {
 
     int somma = 0;
     
@@ -49,5 +93,6 @@ int sommaElementiArray(int[] array) {
 
     }
 
+    Console.WriteLine("La somma è: " + somma);
     return somma;
 }
